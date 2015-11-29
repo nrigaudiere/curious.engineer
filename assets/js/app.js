53bf03790087feca643360cdf9300b7d9abc3820
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2015-11-29 22:58:59 +0100

    Changed bg img + edited front + edited js handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
 $(document).foundation();
 
+var open = $('#navbar-open');
+var close = $('#navbar-close');
 
-function toggleMenu() {
-	console.log('Trip');
-}
+open.click(function() {
+	open.addClass( "hide" ).removeClass("show");
+	close.addClass( "show animated pulse" ).removeClass("hide");
+});
+
+close.click(function() {
+	close.addClass( "hide" ).removeClass("show");
+	open.addClass( "show animated pulse" ).removeClass("hide");
+});
+
+
