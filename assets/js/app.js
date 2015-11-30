333edf0a51ee4f0229b3afdcddede6d8a3ad88a3
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2015-11-30 21:45:48 +0100

    Tested new fonts + new background + js edits for responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,16 @@ $(document).foundation();
 
 var open = $('#navbar-open');
 var close = $('#navbar-close');
+var menuo = $('#menu-overlay');
 
 open.click(function() {
 	open.addClass( "hide" ).removeClass("show");
 	close.addClass( "show animated pulse" ).removeClass("hide");
+	menuo.addClass( "show animated fadeInRight" ).removeClass("hide");
 });
 
 close.click(function() {
 	close.addClass( "hide" ).removeClass("show");
+	menuo.addClass( "fadeOutRight" ).removeClass("show");
 	open.addClass( "show animated pulse" ).removeClass("hide");
 });
-
-
