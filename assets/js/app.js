d2f8299646f10969938ad520b9b2e9782a197711
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2015-12-03 00:08:28 +0100

    Edited background images + js handle

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,15 @@ var close = $('#navbar-close');
 var menuo = $('#menu-overlay');
 
 open.click(function() {
-	open.addClass( "hide" ).removeClass("show");
+	open.addClass( "hide" ).removeClass("show animated pulse");
 	close.addClass( "show animated pulse" ).removeClass("hide");
-	menuo.addClass( "show animated fadeInRight" ).removeClass("hide");
+	menuo.addClass( "show animated fadeInRight" ).removeClass("hide fadeOutRight");
 });
 
 close.click(function() {
-	close.addClass( "hide" ).removeClass("show");
-	menuo.addClass( "fadeOutRight" ).removeClass("show");
+	close.addClass( "hide" ).removeClass("show animated pulse");
+	menuo.addClass( "fadeOutRight" ).removeClass("show animated fadeInRight");
 	open.addClass( "show animated pulse" ).removeClass("hide");
 });
+
+
