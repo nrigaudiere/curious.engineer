2282467682907d46e95bbca13ef169c364662310
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2015-12-06 18:16:07 +0100

    Added responsive menu + edited all menus

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,17 @@ $(document).foundation();
 
 var open = $('#navbar-open');
 var close = $('#navbar-close');
+var menub = $('.menu-bar');
 var menuo = $('#menu-overlay');
 
 open.click(function() {
-	open.addClass( "hide" ).removeClass("show animated pulse");
-	close.addClass( "show animated pulse" ).removeClass("hide");
+	open.addClass( "animated fadeOut" ).removeClass("fadeIn");
 	menuo.addClass( "show animated fadeInRight" ).removeClass("hide fadeOutRight");
 });
 
 close.click(function() {
-	close.addClass( "hide" ).removeClass("show animated pulse");
-	menuo.addClass( "fadeOutRight" ).removeClass("show animated fadeInRight");
-	open.addClass( "show animated pulse" ).removeClass("hide");
+	menuo.removeClass("show animated fadeInRight").addClass("animated fadeOutRight" );
+	open.addClass( "show animated fadeIn" ).removeClass("fadeOut");
 });
 
 
