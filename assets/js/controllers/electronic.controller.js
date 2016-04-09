6ff549be0b63ca2acd493caf612563649ee14725
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2016-04-09 19:07:09 +0200

    Revert "Added AngularJS"
    This reverts commit 09fa0de4868da4ee91876a7778c00703042c17d3.

--- assets/js/controllers/electronic.controller.js
+++
@@ -1,9 +0,0 @@
-aCuriousEngineer.controller('electronic.controller', function ($scope, $http) {
-  $scope.display = true;
-
-  $http.get("../assets/json/electronic.projects.json").success(function(data) {
-    $scope.projects = data;
-    console.log($scope.projects);
-	});
-
-});
--- assets/js/main.js
+++
@@ -1,2 +0,0 @@
-var aCuriousEngineer = angular.module('aCuriousEngineer', []);
-
