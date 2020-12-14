8d92ca492f3c00e5d4ab910a23d81f169966c397
Nicolas Rigaudiere <i.am.a@curious.engineer>
2020-12-14 23:38:06 +0100

    feat: create root

--- src/components/ace-root.tsx
+++ src/components/ace-root.tsx
@@ -1,22 +1,21 @@
 import { Component, h } from '@stencil/core';
 
 @Component({
-  tag: 'app-root',
-  styleUrl: 'app-root.css',
-  shadow: true,
+  tag: 'ace-root',
 })
-export class AppRoot {
+export class ACERoot {
   render() {
     return (
       <div>
         <header>
-          <h1>Stencil App Starter</h1>
+
         </header>
 
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url="/" component="app-home" exact={true} />
+              <stencil-route url="/" component="ace-home" exact={true} />
+              <stencil-route url="/work" component="ace-work" componentProps={ { name: "toto" } } exact={true} />
             </stencil-route-switch>
           </stencil-router>
         </main>
